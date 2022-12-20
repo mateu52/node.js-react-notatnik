24da918bece5d294467a34728a7ed3494e35529b
mateu52 <wuoelte@gmail.com>
2022-12-20 20:50:32 +0100

    express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const config = require('./config');
+const apiRouter = require('./routes/api');
+
+
+//routes
+app.use('/', apiRouter );
+
+
+app.listen(config.port, function() {
+    console.log('serwer słucha...htttp:.3000..' + config.port)
+});
